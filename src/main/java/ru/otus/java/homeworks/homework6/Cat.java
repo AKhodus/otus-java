ba643e6c0ccc48c3978a23f790590f456ae639f8
Alexander Khodus <khodus-sasha@yandex.ru>
2024-02-19 10:21:01 +0300

    Правки внесены

--- src/main/java/ru/otus/java/homeworks/homework6/Cat.java
+++ src/main/java/ru/otus/java/homeworks/homework6/Cat.java
@@ -2,7 +2,7 @@ package ru.otus.java.homeworks.homework6;
 
 public class Cat {
     private String name;
-    private boolean fulness = false;
+    private boolean fulness;
 
     public Cat(String name) {
         this.name = name;
@@ -20,9 +20,9 @@ public class Cat {
         System.out.println("Сытость " + name + ": " + fulness);
     }
 
-    public void eat(Plate plate1, int count) {
-        if ((plate1.getCurrentFood() - count) >= 0) {
-            plate1.setCurrentFood(plate1.getCurrentFood() - count);
+    public void eat(Plate plate, int count) {
+        if ((plate.getCurrentFood() - count) >= 0) {
+            plate.setCurrentFood(plate.getCurrentFood() - count);
             fulness = true;
             System.out.println(name + " поел");
         } else {
--- src/main/java/ru/otus/java/homeworks/homework6/Main.java
+++ src/main/java/ru/otus/java/homeworks/homework6/Main.java
@@ -2,25 +2,25 @@ package ru.otus.java.homeworks.homework6;
 
 public class Main {
     public static void main(String[] args) {
-        Plate plate1 = new Plate(100, 100);
+        Plate plate = new Plate(100, 100);
         Cat[] cats = {
                 new Cat("Barsik"),
                 new Cat("Rizhik"),
                 new Cat("Murzik")
         };
-        //Cat cat1 = new Cat("Barsik");
-        //plate1.fillPlate();
-        //plate1.addFood(50);
-        //plate1.info();
-        //plate1.decreaseFood(50);
-        //plate1.info();
-        //cat1.eat(plate1,50);
-        //cat1.info();
-        //plate1.info();
+        //Cat cat = new Cat("Barsik");
+        //plate.fillPlate();
+        //plate.addFood(50);
+        //plate.info();
+        //plate.decreaseFood(50);
+        //plate.info();
+        //cat.eat(plate1,50);
+        //cat.info();
+        //plate.info();
         for (int i = 0; i < cats.length; i++) {
-            cats[i].eat(plate1, 50);
+            cats[i].eat(plate, 50);
             cats[i].info();
-            plate1.info();
+            plate.info();
         }
     }
 
