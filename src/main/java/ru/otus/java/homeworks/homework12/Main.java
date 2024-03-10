58c88f366228b640a36ada13eca55931c6956db4
Alexander Khodus <khas@arenadata.io>
2024-03-10 15:05:58 +0300

    Внесены правки

--- src/main/java/ru/otus/java/homeworks/homework12/Main.java
+++ src/main/java/ru/otus/java/homeworks/homework12/Main.java
@@ -1,7 +1,14 @@
 package ru.otus.java.homeworks.homework12;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        TextFileEditor.textFile();
+        TextFileEditor.listTextFiles();
+        File selectedFile = TextFileEditor.selectFile();
+        if (selectedFile != null) {
+            TextFileEditor.readFile(selectedFile);
+            TextFileEditor.appendToFile(selectedFile);
+        }
     }
 }
--- src/main/java/ru/otus/java/homeworks/homework12/TextFileEditor.java
+++ src/main/java/ru/otus/java/homeworks/homework12/TextFileEditor.java
@@ -8,8 +8,8 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class TextFileEditor {
-    public static void textFile() {
-        String rootPath = System.getProperty("user.dir");
+    public static void listTextFiles() {
+        String rootPath = ".";
         File rootDir = new File(rootPath);
         File[] files = rootDir.listFiles((dir, name) -> name.endsWith(".txt"));
 
@@ -18,40 +18,50 @@ public class TextFileEditor {
             for (File file : files) {
                 System.out.println(file.getName());
             }
-
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Введите имя файла, с которым хотите работать: ");
-            String fileName = scanner.nextLine();
-
-            File selectedFile = new File(rootPath, fileName);
-
-            if (!selectedFile.exists()) {
-                System.out.println("Файл не найден.");
-            } else {
-                try (BufferedReader reader = new BufferedReader(new FileReader(selectedFile))) {
-                    System.out.println("Содержимое файла:");
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        System.out.println(line);
-                    }
-
-                    System.out.print("Введите строку для записи в файл: ");
-                    String content = scanner.nextLine();
-
-                    try (FileWriter writer = new FileWriter(selectedFile, true)) {
-                        content += "\n";
-                        writer.write(content);
-                        System.out.println("Строка успешно записана в файл.");
-                    } catch (IOException e) {
-                        System.out.println("Ошибка записи в файл: " + e.getMessage());
-                    }
-
-                } catch (IOException e) {
-                    System.out.println("Ошибка чтения файла: " + e.getMessage());
-                }
-            }
         } else {
             System.out.println("В корневом каталоге проекта нет текстовых файлов.");
+        }
+    }
+
+    public static File selectFile() {
+        String rootPath = ".";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите имя файла, с которым хотите работать: ");
+        String fileName = scanner.nextLine();
+
+        File selectedFile = new File(rootPath, fileName);
+
+        if (!selectedFile.exists()) {
+            System.out.println("Файл не найден.");
+            return null;
+        } else {
+            return selectedFile;
+        }
+    }
+
+    public static void readFile(File selectedFile) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(selectedFile))) {
+            System.out.println("Содержимое файла:");
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка чтения файла: " + e.getMessage());
+        }
+    }
+
+    public static void appendToFile(File selectedFile) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите строку для записи в файл: ");
+        String content = scanner.nextLine();
+
+        try (FileWriter writer = new FileWriter(selectedFile, true)) {
+            content += "\n";
+            writer.write(content);
+            System.out.println("Строка успешно записана в файл.");
+        } catch (IOException e) {
+            System.out.println("Ошибка записи в файл: " + e.getMessage());
         }
     }
 }
