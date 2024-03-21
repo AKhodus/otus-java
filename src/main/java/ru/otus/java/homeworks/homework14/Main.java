71e3db3759de91386e3f7b1a6c513be1ceaa8848
Alexander Khodus <khodus-sasha@yandex.ru>
2024-03-21 09:17:10 +0300

    Внесены правки

--- src/main/java/ru/otus/java/homeworks/homework14/Main.java
+++ src/main/java/ru/otus/java/homeworks/homework14/Main.java
@@ -3,9 +3,15 @@ package ru.otus.java.homeworks.homework14;
 public class Main {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        //double[] array = Implementation1.createArray();
-        double[] array = Implementation2.createArray();
+        double[] array = Implementation1.createArray();
         long endTime = System.currentTimeMillis();
-        System.out.println("Execution time: " + (endTime - startTime) / 1000 + " sec");
+        int result = (int) (endTime - startTime);
+        System.out.println("Execution time: " + result / 1000 + " sec");
+        long startTime1 = System.currentTimeMillis();
+        double[] array1 = Implementation2.createArray();
+        long endTime1 = System.currentTimeMillis();
+        int result1 = (int) (endTime1 - startTime1);
+        System.out.println("Execution time: " + result1 / 1000 + " sec");
+        System.out.println("Execution time difference: " + (result - result1) / 1000 + " sec");
     }
 }
