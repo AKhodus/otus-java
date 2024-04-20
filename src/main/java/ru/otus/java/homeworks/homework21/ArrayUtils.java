ad605bb7c3675c2c98e03195e8f159133f0737d7
Alexander Khodus <khas@arenadata.io>
2024-04-20 16:37:14 +0300

    Домашнее задание 21 выполнено

---
+++ src/main/java/ru/otus/java/homeworks/homework21/ArrayUtils.java
@@ -0,0 +1,34 @@
+package ru.otus.java.homeworks.homework21;
+
+public class ArrayUtils {
+
+    // Метод для возвращения подмассива после последней единицы
+    public static int[] getSubarrayAfterLastOne(int[] array) {
+        int lastOneIndex = -1;
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (array[i] == 1) {
+                lastOneIndex = i;
+                break;
+            }
+        }
+
+        if (lastOneIndex == -1) {
+            throw new RuntimeException("Input array does not contain any ones.");
+        }
+
+        int subarrayLength = array.length - lastOneIndex - 1;
+        int[] subarray = new int[subarrayLength];
+        System.arraycopy(array, lastOneIndex + 1, subarray, 0, subarrayLength);
+        return subarray;
+    }
+
+    // Метод для проверки, состоит ли массив только из чисел 1 и 2
+    public static boolean containsOnlyOnesAndTwos(int[] array) {
+        for (int num : array) {
+            if (num != 1 && num != 2) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
