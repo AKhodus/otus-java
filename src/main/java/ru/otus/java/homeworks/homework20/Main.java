3cf7db090df33d68efff3ab2640f2280176f4f9d
Alexander Khodus <khas@arenadata.io>
2024-04-23 08:46:43 +0300

    Внесены правки

--- src/main/java/ru/otus/java/homeworks/homework20/Main.java
+++ src/main/java/ru/otus/java/homeworks/homework20/Main.java
@@ -1,5 +1,7 @@
 package ru.otus.java.homeworks.homework20;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -7,52 +9,17 @@ public class Main {
     public static void main(String[] args) {
         ExecutorService threadPool = Executors.newFixedThreadPool(3);
 
-        // Создаем объект-монитор для синхронизации потоков
+        List<Character> characters = new ArrayList<>();
+        characters.add('A');
+        characters.add('B');
+        characters.add('C');
+
         Object monitor = new Object();
 
-        threadPool.submit(new PrintTask('A', monitor, 5));
-        threadPool.submit(new PrintTask('B', monitor, 5));
-        threadPool.submit(new PrintTask('C', monitor, 5));
+        for (int i = 0; i < characters.size(); i++) {
+            threadPool.submit(new PrintTask(characters.get(i), monitor));
+        }
 
         threadPool.shutdown();
-    }
-
-    static class PrintTask implements Runnable {
-        private char charToPrint;
-        private Object monitor;
-        private int times;
-
-        public PrintTask(char charToPrint, Object monitor, int times) {
-            this.charToPrint = charToPrint;
-            this.monitor = monitor;
-            this.times = times;
-        }
-
-        @Override
-        public void run() {
-            for (int i = 0; i < times; i++) {
-                synchronized (monitor) {
-                    try {
-                        // Проверяем, если необходимо печатать текущий символ
-                        while (charToPrint != 'A' && charToPrint != 'B' && charToPrint != 'C') {
-                            monitor.wait();
-                        }
-
-                        // Печатаем символ и оповещаем другие потоки
-                        System.out.print(charToPrint);
-                        if (charToPrint == 'A') {
-                            charToPrint = 'B';
-                        } else if (charToPrint == 'B') {
-                            charToPrint = 'C';
-                        } else {
-                            charToPrint = 'A';
-                        }
-                        monitor.notifyAll();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
     }
 }
